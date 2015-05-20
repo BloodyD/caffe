ea8e7b6d0b6b69a4c6c57ff705df41e8cf68cfb0
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-05-20 15:34:35 +0200

    make it build and call super class setup and reshape

--- src/caffe/layers/multi_classifier_loss_layer.cpp
+++ src/caffe/layers/multi_classifier_loss_layer.cpp
@@ -6,37 +6,37 @@
 namespace caffe {
 
 template <typename Dtype>
-void MultiClassifierLossLayer::LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+void MultiClassifierLossLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+{
+    LossLayer<Dtype>::LayerSetUp(bottom, top);
+}
+
+template <typename Dtype>
+void MultiClassifierLossLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
 {
     // TODO
 }
 
 template <typename Dtype>
-void MultiClassifierLossLayer::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+void MultiClassifierLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
 {
     // TODO
 }
 
 template <typename Dtype>
-void MultiClassifierLossLayer::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+void MultiClassifierLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
 {
     // TODO
 }
 
 template <typename Dtype>
-void MultiClassifierLossLayer::Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+void MultiClassifierLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
 {
     // TODO
 }
 
 template <typename Dtype>
-void MultiClassifierLossLayer::Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
-{
-    // TODO
-}
-
-template <typename Dtype>
-void MultiClassifierLossLayer::Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
+void MultiClassifierLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
 {
     // TODO
 }
